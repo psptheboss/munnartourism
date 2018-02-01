6ce1cd9ebdc90c5109d63047d4961f4ab41caf2d
Palavesam <pspalavesam@gmail.com>
2018-02-01 12:50:03 +0530

    ff

--- src/app/pages/dashboard/calendar/calendar.component.ts
+++ src/app/pages/dashboard/calendar/calendar.component.ts
@@ -15,27 +15,11 @@ export class Calendar {
 
   constructor(private _calendarService:CalendarService) {
     this.calendarConfiguration = this._calendarService.getData();
-    this.calendarConfiguration.select = (start, end) => this._onSelect(start, end);
+
   }
 
   public onCalendarReady(calendar):void {
     this._calendar = calendar;
   }
 
-  private _onSelect(start, end):void {
-
-    if (this._calendar != null) {
-      let title = prompt('Event Title:');
-      let eventData;
-      if (title) {
-        eventData = {
-          title: title,
-          start: start,
-          end: end
-        };
-        jQuery(this._calendar).fullCalendar('renderEvent', eventData, true);
-      }
-      jQuery(this._calendar).fullCalendar('unselect');
-    }
-  }
 }
